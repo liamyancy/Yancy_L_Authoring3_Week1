36c8c87aac82a79f3992be7b003b56a3b8688a45
liamyancy <55156307+liamyancy@users.noreply.github.com>
2020-10-01 14:47:38 -0400

    Adding Dynamic Content

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-(())
+import Team from "./modules/DataModule.js";
+
+(() => {
+    console.log('loaded');
+
+    console.log(Team);
+
+    //debugger;
+
+    // select our user elements and load the content
+    let userSection = document.querySelector(".user-section").children;
+
+    userSection[1].textContent = Team["Justin"].name;
+    userSection[2].textContent = Team["Justin"].role;
+    userSection[3].textContent = Team["Justin"].nickname;
+})();
